1ad961a574ca9ffe274115f695f5dd0d5ba4143b
Sastry Duri <sastry@us.ibm.com>
2016-06-16 04:48:09 -0400

    fail test

--- src/test_add.py
+++ src/test_add.py
@@ -4,7 +4,7 @@ import add
 class My_tests(unittest.TestCase):
 
     def test_add(self):
-        self.assertEqual(7, add.add(3,4))
+        self.assertEqual(8, add.add(3,4))
 
 if __name__ == '__main__':
     unittest.main()
